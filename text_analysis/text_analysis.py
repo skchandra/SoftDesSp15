3d2bf6344bd1fb44707175a8af369d43840fb316
skchandra <shivali.chandra@students.olin.edu>
2015-02-26 02:17:34 -0500

    Final text analysis code and project write-up

--- text_analysis/text_analysis.py
+++ text_analysis/text_analysis.py
@@ -8,6 +8,7 @@ import plotly.plotly as py
 from plotly.graph_objs import *
 from pattern.en import *
 import re
+import csv
 
 def top_words(dictionary,num):
 	"""This function takes a dictionary and a number, and locates the most frequency words, 
@@ -27,7 +28,6 @@ def top_words(dictionary,num):
 		k.remove(key)
 		v.remove(val)
 	return new_dict
-import csv
 
 def plot_frequency(dictionary,name):
 	"""This function uses top_words to find the 30 highest words, then plots the words and their word count"""
@@ -35,9 +35,9 @@ def plot_frequency(dictionary,name):
 	x = freq_words.keys()
 	y = freq_words.values()
 	#used the following code to write the dictionary to a file to create bubble plots online
-	w = csv.writer(open("output2.csv", "w"))
+	"""w = csv.writer(open("output2.csv", "w"))
 	for key, val in freq_words.items():
-   		w.writerow([key, val])
+   		w.writerow([key, val])"""
    	#code to plot bargraphs 
 	data = Data([
 	    Bar(
@@ -45,18 +45,49 @@ def plot_frequency(dictionary,name):
 	        y=y
 	    )
 	])
-	plot_url = py.plot(data, filename=name)
+	layout = Layout(
+	    title='Character Mentions in A Dance With Dragons',
+	    xaxis=XAxis(
+	        title='Character Name',
+	        titlefont=Font(
+	            size=16,
+	            color='rgb(107, 107, 107)'
+	        ),
+	        tickfont=Font(
+	            size=14,
+	            color='rgb(107, 107, 107)'
+	        )
+	    ),
+	    yaxis=YAxis(
+	        title='Number of Mentions',
+	        titlefont=Font(
+	            size=16,
+	            color='rgb(107, 107, 107)'
+	        ),
+	        tickfont=Font(
+	            size=14,
+	            color='rgb(107, 107, 107)'
+	        )
+	    ),
+	    barmode='stack',
+	    bargap=0.15,
+	    bargroupgap=0.1
+	)
+	fig = Figure(data=data, layout=layout)
+	plot_url = py.plot(fig, filename=name)
 
 def line_sentiment(alllines,chapter_begins,filename):
 	"""This function determines the sentiment of each line in a text file, and places the lines and their sentiment number into
 	various dictionaries in order to then plot the sentiment ranges in different colors."""
 	lines_chapters = {}
+	#create a dictionary of every line and its corresponding chapter
 	for i in range(0,len(chapter_begins)-1):
 		start = chapter_begins[i]
 		end = chapter_begins[i+1]
 		while start < end:
 			lines_chapters[start] = i+1 
 			start+=1
+	#initialize the dictionaries
 	very_happy = {}
 	happy = {}
 	neutral = {}
@@ -67,6 +98,7 @@ def line_sentiment(alllines,chapter_begins,filename):
 	new_n = {}
 	new_neg = {}
 	new_v_neg = {}
+	#analyze sentiment of each line and place it in a line-sentiment dictionary
 	for i,n in enumerate(alllines):
 		sent = sentiment(n)[0]
 		if sent >= .5:
@@ -79,6 +111,8 @@ def line_sentiment(alllines,chapter_begins,filename):
 			neg[i] = sent
 		else:
 			very_neg[i] = sent
+	#iterate through the line-chapter dictionary keys and find corresponding keys (lines) in the previous dictionaries to create
+	#chapter-sentiment dictionaries
 	for i in lines_chapters.keys():
 		c = lines_chapters.get(i)
 		if i in very_happy.keys():
@@ -91,12 +125,13 @@ def line_sentiment(alllines,chapter_begins,filename):
 			new_neg[c] = new_neg.get(c,0)+1
 		if i in very_neg.keys():
 			new_v_neg[c] = new_v_neg.get(c,0)+1
+	#graph the sentiment
 	#sentiment_by_lines(very_happy,happy,neutral,neg,very_neg,filename)
 	sentiment_by_lines(new_v_h,new_h,new_n,new_neg,new_v_neg,filename)
 
 def sentiment_by_lines(range1,range2,range3,range4,range5,filename):
 	"""This function takes 5 dictionaries and a filename in order to graph a bar graph of line number vs. sentiment"""
-	trace5 = Bar(
+	trace1 = Bar(
 	    x=range1.keys(),
 	    y=range1.values(),
 	    name='Very Positive',
@@ -104,7 +139,7 @@ def sentiment_by_lines(range1,range2,range3,range4,range5,filename):
 	        color='rgb(255,1,1)' 
 	        )
 	    )
-	trace4 = Bar(
+	trace2 = Bar(
 	    x=range2.keys(),
 	    y=range2.values(),
 	    name='Positive',
@@ -120,7 +155,7 @@ def sentiment_by_lines(range1,range2,range3,range4,range5,filename):
 	        color='rgb(255,235,1)'
 	    )
 	)
-	trace2 = Bar(
+	trace4 = Bar(
 	    x=range4.keys(),
 	    y=range4.values(),
 	    name='Negative',
@@ -128,7 +163,7 @@ def sentiment_by_lines(range1,range2,range3,range4,range5,filename):
 	        color='rgb(1,226,34)'
 	    )
 	)
-	trace1 = Bar(
+	trace5 = Bar(
 	    x=range5.keys(),
 	    y=range5.values(),
 	    name='Very Negative',
@@ -138,10 +173,7 @@ def sentiment_by_lines(range1,range2,range3,range4,range5,filename):
 	)
 	data = Data([trace1,trace2,trace3,trace4,trace5])
 	layout = Layout(
-		autosize=False,
-    	width=2500,
-    	height=700,
-	    title='Sentiment Analysis of A Game Of Thrones',
+	    title='Sentiment Analysis of A Game of Thrones by Chapter',
 	    xaxis=XAxis(
 	        title='Chapter',
 	        titlefont=Font(
@@ -182,8 +214,10 @@ def book_analysis(filename):
 	place_count = {}
 	count = 0 
 	chapters = []
+	#lists of names and places to exclude in making lowercase and to find character and place mentions
 	names = ['Rickard','Lyarra','Brandon','Catelyn','Tully','Eddard','Ned','Lyanna','Benjen','Robb','Jeyne','Sansa','Arya','Bran','Rickon','Jon','Snow','Stark','Roose','Bolton','Ramsay','Hodor','Osha','Poole','Jojen','Meera','Reed','Maeker','Daeron','Aerion','Maester','Aemon','Aegon','Rhae','Daella','Duncan','Rhalle','Aerys','Rhaella','Elia','Martell','Rhaegar','Viserys','Drogo','Daenerys','Aegon','Rhaego','Targaryen','Arryn','Lysa','Robert','Jon Arryn','Lannister','Tywin','Cersei','Jaime','Joffrey','Baratheon','Myrcella','Tommen','Tyrion','Kevan','Lancel','Bronn','Gregor','Clegane','Sandor','Podrick','Pod','Steffon','Stannis','Selyse','Renly','Margaery','Tyrell','Mya','Gendry','Edric','Shireen','Stone','Melisandre','Davos','Seaworth','Brienne','Beric','Dondarrion','Greyjoy','Balon','Theon','Asha','Euron','Victarion','Aeron','Doran','Arianne','Quentyn','Trystane','Obara','Oberyn','Nymeria','Tyene','Sarella','Ellaria','Areo','Hotah','Petyr','Baelish','Edmure','Roslin','Brynden','Walder','Loras','Mace','Olenna','Jeor','Mormont','Yoren','Samwell','Sam','Tarly','Janos','Slynt','Alliser','Thorne','Mance','Rayder','Ygritte','Val','Bowen','Varys','Pycelle','Barristan','Selmy','Arys','Oakheart','Ilyn','Payne','Qyburn','Khal','Syrio','Forel','Jaqen','H"ghar','Illyrio','Mopatis','Thoros','Duncan','Ser']
   	places = ['Westeros','Essos','Braavos','Vaes Dothrak','White Harbor','Lys','The North','Duskendale','Black Bay','Lhazosh','Iron Islands','Vale','Westerlands','Stormlands','The Reach','Dorne','Seven Kingdoms','Qohor','Slaver"s Bay','Shivering Sea','Summer Sea','Valyria','Astapor','Yunkai','Meereen','Sothoros','The Neck','Winterfell','The Wall','Beyond the Wall','Iron Islands','Pyke','Great Wyk','Old Wyk','Harlaw','Saltcliffe','Blacktyde','Orkmont','Riverlands','Trident','Harrenhal','Riverrun','Tumblestone River','Red Fork','The Twins','Eyrie','Bloody Gates','Alyssa"s Tears','Casterly Rock','Lannisport','Highgarden','Oldtown','Citadel','Hightower','Storm"s End','Crownlands','Dragonstone','King"s Landing','Red Keep','Iron Throne','Sunspear','Free Cities','Lys','Myr','Pentos','Lorath','Norvos','Volantis','Tyrosh','Dothraki Sea','Rhoyne','Lhazar','Red Waste','Qarth','Worm River','Plaza of Pride','Asshai','Shadow Lands','Ibben','Summer Islands','Yi Ti']
+  	#find five line breaks in a row, which make up the beginning of a new chapter
   	for line in range(0,len(lines)):
   		if (lines[line] in ['\n','\r\n']):
   			count+=1
@@ -193,18 +227,21 @@ def book_analysis(filename):
   		else:
   			count = 0
   		words = lines[line].split()
+  		#remove the punctuation from the words
   		for word in words:
   			punctuation = re.compile(r'[.?!,":;-]') 
   			word = punctuation.sub('', word)
+  			#if not a name or place, make lowercase
   			if word not in names+places:
 				word = word.lower()
 			word_count[word] = word_count.get(word,0)+1
-	for i in names:
-		if i in words:
-			name_count[i] = name_count.get(i,0)+1
-	for j in places:
-		if j in words:
-			place_count[j] = place_count.get(j,0)+1
+		#count frequency of names and places
+		for i in names:
+			if i in words:
+				name_count[i] = name_count.get(i,0)+1
+		for j in places:
+			if j in words:
+				place_count[j] = place_count.get(j,0)+1
 	#plot_frequency(name_count,filename)
 	#plot_frequency(place_count,filename)
 	#plot_frequency(word_count,filename)
