b0cca68cd2c6b34d7eb5eee66652d21f9fcedff1
skchandra <shivali.chandra@students.olin.edu>
2015-03-30 22:41:39 -0400

    Changed format of code to be more modular, used functions

--- finalProjectWorkDir/2D3D.py
+++ finalProjectWorkDir/2D3D.py
@@ -4,94 +4,94 @@ Function that takes in lists of tuples that represent 2D-coordinates and convert
 import plotly.plotly as py
 from plotly.graph_objs import *
 
-def two_D_three_D(front,left_side,back,right_side,top,bottom):
-	#front plane
-	front_3D = [(front[0][0],front[0][1],0),(front[1][0],front[1][1],0),(front[2][0],front[2][1],0),(front[3][0],front[3][1],0)]
-	#defining x-coordinates and lengths for left and right sides respectively
-	start_x_bottom = front_3D[0][0]
-	start_x_top = front_3D[3][0]
-	length_top = front_3D[2][0]-front_3D[3][0]
-	length_bottom = front_3D[1][0]-front_3D[0][0]
-	#left side, x taken from front plane, its x coordinate becomes z
-	left_side_3D = [(start_x_bottom,left_side[0][1],left_side[1][0]),(start_x_bottom,left_side[1][1],left_side[0][0]),(start_x_top,left_side[2][1],left_side[3][0]),(start_x_top,left_side[3][1],left_side[2][0])]
-	#defining left widths for back plane z coordinates
-	left_width_bottom = left_side_3D[0][2]-left_side_3D[1][2]
-	left_width_top = left_side_3D[3][2]-left_side_3D[2][2]
-	right_side_3D = [(length_bottom,right_side[0][1],right_side[0][0]),(length_bottom,right_side[1][1],right_side[1][0]),(length_top,right_side[2][1],right_side[2][0]),(length_top,right_side[3][1],right_side[3][0])]
-	#defining right widths for back plane z coordinates
-	right_width_bottom = right_side_3D[1][2]-right_side_3D[0][2]
-	right_width_top = right_side_3D[2][2]-right_side_3D[3][2]
-	#back side, z taken from left and right sides
-	back_3D = [(back[1][0],back[0][1],right_width_bottom),(back[0][0],back[1][1],left_width_bottom),(back[3][0],back[2][1],left_width_top),(back[2][0],back[3][1],right_width_top)]
-	#defining heights for top plane
-	front_left_height = front_3D[3][1]-front_3D[0][1]
-	front_right_height = front_3D[2][1]-front_3D[1][1]
-	back_left_height = back_3D[2][1]-back_3D[1][1]
-	back_right_height = back_3D[3][1]-back_3D[0][1]
-	#top side, y taken from heights
-	top_3D = [(top[0][0],front_left_height,top[0][1]),(top[1][0],front_right_height,top[1][1]),(top[2][0],back_right_height,top[2][1]),(top[3][0],back_left_height,top[3][1])]
-	#defining y-coordinates for bottom side
-	front_left_y = front_3D[0][1]
-	front_right_y = front_3D[1][1] 
-	back_left_y = back_3D[1][1]
-	back_right_y = back_3D[0][1]
-	#bottom side, y taken from four variables above
-	bottom_3D = [(bottom[0][0],back_left_y,bottom[3][1]),(bottom[1][0],back_right_y,bottom[1][1]),(bottom[2][0],front_right_y,bottom[2][1]),(bottom[3][0],front_left_y,bottom[0][1])]
-	
+def front_plane(front):
+	new_front = [None]*len(front)
+	for i,(x,y) in enumerate(front):
+		new_front[i] = (x,y,0)
+	return new_front
 
-	#b = [int(i[0]) for i in front_3D]
-	c = [int(i[1]) for i in front_3D]
-	d = [int(i[2]) for i in front_3D]
-	b1 = [int(i[0]) for i in left_side_3D]
-	c1 = [int(i[1]) for i in left_side_3D]
-	d1 = [int(i[2]) for i in left_side_3D]
-	b2 = [int(i[0]) for i in back_3D]
-	c2 = [int(i[1]) for i in back_3D]
-	d2 = [int(i[2]) for i in back_3D]
-	b3 = [int(i[0]) for i in right_side_3D]
-	c3 = [int(i[1]) for i in right_side_3D]
-	d3 = [int(i[2]) for i in right_side_3D]
-	b4 = [int(i[0]) for i in top_3D]
-	c4 = [int(i[1]) for i in top_3D]
-	d4 = [int(i[2]) for i in top_3D]
-	b5 = [int(i[0]) for i in bottom_3D]
-	c5 = [int(i[1]) for i in bottom_3D]
-	d5 = [int(i[2]) for i in bottom_3D]
+def left_plane(left_side,start_x):
+	left = [None]*len(left_side)
+	for i,(x,y) in enumerate(left_side):
+		left[i] = (start_x,y,x)
+	left_list = [list(k) for k in left]
+	left_list[0][2],left_list[1][2],left_list[2][2],left_list[3][2] = left_list[1][2],left_list[0][2],left_list[3][2],left_list[2][2]
+	new_left = [tuple(j) for j in left_list]
+	return new_left
+
+def right_plane(right_side,width):
+	new_right = [None]*len(right_side)
+	for i,(x,y) in enumerate(right_side):
+		new_right[i] = (width,y,x)
+	return new_right
+
+def back_plane(back,length):
+	back_side = [None]*len(back)
+	for i,(x,y) in enumerate(back):
+		back_side[i] = (x,y,length)
+	back_list = [list(k) for k in back_side]
+	back_list[0][0],back_list[1][0],back_list[2][0],back_list[3][0] = back_list[1][0],back_list[0][0],back_list[3][0],back_list[2][0]
+	new_back = [tuple(j) for j in back_list]
+	return new_back
+
+def top_plane(top,height):
+	new_top = [None]*len(top)
+	for i,(x,y) in enumerate(top):
+		new_top[i] = (x,height,y)
+	return new_top
+
+def bottom_plane(bottom,start_y):
+	bottom_side = [None]*len(bottom)
+	for i,(x,y) in enumerate(bottom):
+		bottom_side[i] = (x,start_y,y)
+	bottom_list = [list(k) for k in bottom_side]
+	bottom_list[0][2],bottom_list[2][2],bottom_list[1][2],bottom_list[3][2] = bottom_list[2][2],bottom_list[0][2],bottom_list[3][2],bottom_list[1][2]
+	new_bottom = [tuple(j) for j in bottom_list]
+	return new_bottom
+
+def make_shape(front,left_side,back,right_side,top,bottom):
+	front_3D = front_plane(front)
+	left_side_3D = left_plane(left_side,front_3D[0][0])
+	right_side_3D = right_plane(right_side,front_3D[1][0])
+	back_3D = back_plane(back,left_side_3D[0][2])
+	top_3D = top_plane(top,front_3D[2][1])
+	bottom_3D = bottom_plane(bottom,front_3D[0][1])
+
 	trace1 = Scatter3d(
 	    x=[int(i[0]) for i in front_3D],
-	    y=c,
-	    z=d,
-	    mode='markers',
+	    y=[int(j[1]) for j in front_3D],
+	    z=[int(k[2]) for k in front_3D],
+	    mode='lines+markers',
 	)
 	trace2 = Scatter3d(
-	    x=b1,
-	    y=c1,
-	    z=d1,
-	    mode='markers',
+	    x=[int(i[0]) for i in left_side_3D],
+	    y=[int(j[1]) for j in left_side_3D],
+	    z=[int(k[2]) for k in left_side_3D],
+	    mode='lines+markers',
 	)
 	trace3 = Scatter3d(
-	    x=b2,
-	    y=c2,
-	    z=d2,
-	    mode='markers',
+	    x=[int(i[0]) for i in right_side_3D],
+	    y=[int(j[1]) for j in right_side_3D],
+	    z=[int(k[2]) for k in right_side_3D],
+	    mode='lines+markers',
 	)
 	trace4 = Scatter3d(
-	    x=b3,
-	    y=c3,
-	    z=d3,
-	    mode='markers',
+	    x=[int(i[0]) for i in back_3D],
+	    y=[int(j[1]) for j in back_3D],
+	    z=[int(k[2]) for k in back_3D],
+	    mode='lines+markers',
 	)
 	trace5 = Scatter3d(
-	    x=b4,
-	    y=c4,
-	    z=d4,
-	    mode='markers',
+	    x=[int(i[0]) for i in top_3D],
+	    y=[int(j[1]) for j in top_3D],
+	    z=[int(k[2]) for k in top_3D],
+	    mode='lines+markers',
 	)
 	trace6 = Scatter3d(
-	    x=b5,
-	    y=c5,
-	    z=d5,
-	    mode='markers',
+	    x=[int(i[0]) for i in bottom_3D],
+	    y=[int(j[1]) for j in bottom_3D],
+	    z=[int(k[2]) for k in bottom_3D],
+	    mode='lines+markers',
 	)
 	data = Data([trace1,trace2,trace3,trace4,trace5,trace6])
 	layout = Layout(
@@ -105,13 +105,8 @@ def two_D_three_D(front,left_side,back,right_side,top,bottom):
 	fig = Figure(data=data, layout=layout)
 	plot_url = py.plot(fig, filename='simple-3d-scatter')
 
-	#return "front:",front_3D,"left:",left_side_3D,"back:",back_3D,"right:",right_side_3D,"top:",top_3D,"bottom:",bottom_3D
-	print "front:",front_3D
-	print "left:",left_side_3D
-	print "back:",back_3D
-	print "right:",right_side_3D
-	print "top:",top_3D
-	print "bottom:",bottom_3D
+	return "front:",front_3D,"left:",left_side_3D,"back:",back_3D,"right:",right_side_3D,"top:",top_3D,"bottom:",bottom_3D
+
 
 front_2D = [(0,0),(5,0),(5,5),(0,5)]
 left_side_2D = [(0,0),(5,0),(5,5),(0,5)]
@@ -119,4 +114,4 @@ back_2D = [(0,0),(5,0),(5,5),(0,5)]
 right_side_2D = [(0,0),(5,0),(5,5),(0,5)]
 top_2D = [(0,0),(5,0),(5,5),(0,5)]
 bottom_2D = [(0,0),(5,0),(5,5),(0,5)]
-print two_D_three_D(front_2D,left_side_2D,back_2D,right_side_2D,top_2D,bottom_2D)
+print make_shape(front_2D,left_side_2D,back_2D,right_side_2D,top_2D,bottom_2D)
