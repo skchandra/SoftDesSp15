47b11bd37dc1142865da645bf5da424d3a9fab82
skchandra <shivali.chandra@students.olin.edu>
2015-04-23 00:09:39 -0400

    Pushing before making changes to address rotation of sides

--- finalProjectWorkDir/folding_v2.py
+++ finalProjectWorkDir/folding_v2.py
@@ -5,24 +5,44 @@ until they meet, forming a closed 3D shape.
 
 import numpy as np
 import math
-import collections
 
-def move_to_actual_coord(old_side,side_dict,side_num):
+def move_to_actual_coord(old_side,side_dict,side_num,theta):
 	"""Change the xy coordinates of the sides to the correct values from the original image
 		Input: side coordinates, sides dictionary
 		Output: new side coordinates in the sides dictionary	
 	"""
+	final_side = list()
 	move_side = list()
-	for i in old_side:
-		point_num = old_side.index(i)
-		new_x = side_dict[side_num][1][point_num][0]
-		x_diff = side_dict[side_num][0][point_num][0] - i[0]
-		new_y = side_dict[side_num][1][point_num][1]
-		y_diff = side_dict[side_num][0][point_num][1] - i[1]
+	rotate = 0
+	yes = False
+	no = False
+	if side_dict[side_num][1][0][1]<side_dict[side_num][1][len(old_side)-1][1]:
+		rotate = 6
+	for j,i in enumerate(old_side):
+		new_x = side_dict[side_num][1][j][0]
+		#rotate side coordinates if needed
+		if rotate > 0:
+			print side_dict[side_num][1],
+			yes = True
+		while rotate > 0:
+			side_dict[side_num][1] = np.roll(side_dict[side_num][1],1)
+			rotate-=1
+		if yes:
+			print side_dict[side_num][1]
+			no = True
+		x_diff = side_dict[side_num][0][j][0] - i[0]
+		x_diff = 0
+		new_y = side_dict[side_num][1][j][1]
+		y_diff = side_dict[side_num][0][j][1] - i[1]
+		y_diff = 0
 		z = i[2]
-		final_side = new_x-x_diff,new_y-y_diff,z
-		side_dict[side_num].append(final_side)
-	print side_dict
+		final_coord = new_x-x_diff,new_y-y_diff,z
+		final_side.append(final_coord)
+	side_dict[side_num].append(final_side)
+	if no:
+		print side_dict[side_num][2]
+		no = False
+		yes = False
 
 def transform_side(side,theta,xy_coord,side_num):
 	"""Transform the coordinates of the side onto the perpendicular plane using Euler-Rodrigues formula
@@ -53,7 +73,7 @@ def transform_side(side,theta,xy_coord,side_num):
 		folded_vector = round(transform_vector[0]),round(transform_vector[1]),round(transform_vector[2])
 		new_side.append(folded_vector)
 
-	moved_side = move_to_actual_coord(new_side,xy_coord,side_num)
+	moved_side = move_to_actual_coord(new_side,xy_coord,side_num,theta)
 	return moved_side
 
 def make_dictionaries(sides,xy_coord):
@@ -69,7 +89,7 @@ def make_dictionaries(sides,xy_coord):
 
 def main(sides,xy_coordinates):
 	"""call things"""
-	theta = 90
+	theta = 0
 	folded_sides = list()
 	length = len(sides)
 	for i in sides:
@@ -77,12 +97,7 @@ def main(sides,xy_coordinates):
 		folded_sides.append(transform_side(side,theta,xy_coordinates,i))
 	return folded_sides
 
-#side_coordinates = (([0,0],[0,6],[6,6],[6,0]),([0,0],[0,6],[6,6],[6,0]),([0,0],[0,6],[6,6],[6,0]),([0,0],[0,6],[6,6],[6,0]))
-#actual_coordinates = (([6,6],[6,0],[0,0],[0,6]),([0,6],[-6,6],[-6,12],[0,12]),([0,12],[0,18],[6,18],[6,12]),([6,12],[12,12],[12,6],[6,6]))
-#actual_fold_lines = ([0,12],[0,18],[0,18],[6,18],[6,18],[6,12],[6,12],[6,6],[0,6])
-
-side_coordinates = (([0,0],[0,6],[6,6],[6,0]),([0,0],[0,6],[6,6],[6,0]))
-actual_coordinates = (([0,12],[0,18],[6,18],[6,12]),([0,6],[-6,6],[-6,12],[0,12]))
-actual_fold_lines = ([0,12],[0,18])
+side_coordinates = (((0,0),(0,6),(6,6),(6,0)),((0,0),(0,6),(6,6),(6,0)),((0,0),(0,6),(6,6),(6,0)),((0,0),(0,6),(6,6),(6,0)))
+actual_coordinates = (((6,6),(0,6),(0,12),(6,12)),((6,12),(6,18),(12,18),(12,12)),((12,12),(18,12),(18,6),(12,6)),((12,6),(12,0),(6,0),(6,6)))
 
 print make_dictionaries(side_coordinates,actual_coordinates)
